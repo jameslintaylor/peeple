da6eb5b7ffec91f233d8b1e47950c97438a19279
jlt <jameslintaylor@gmail.com>
2017-05-02 17:03:43 -0400

    kabang

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-hello
+function love.load()
+ 
+end
+ 
+function love.update(dt)
+ 
+end
+ 
+function love.draw()
+  love.graphics.print("Hello!",400,300)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
+end
+
