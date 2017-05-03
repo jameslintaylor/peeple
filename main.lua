f3ad7502932585f67c69ffbcddd8b0005cfa089f
elainewelsher <everyelaine@hotmail.com>
2017-05-02 20:09:23 -0400

    Did some work

--- main.lua
+++ main.lua
@@ -1,14 +1,37 @@
+evil_img = nil
+normal_img = nil
+peeple = {x = 0, y = 0, is_evil = false}
+
 function love.load()
- 
+
+  evil_img = love.graphics.newImage('pp-06.png')
+  normal_img = love.graphics.newImage('pp-08.png')
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "space" then
+    peeple.is_evil = not peeple.is_evil
+  end
 end
  
 function love.update(dt)
- 
+  speed = peeple.is_evil and 20 or 2
+  if love.keyboard.isDown("left") then
+    peeple.x = peeple.x - speed
+  elseif love.keyboard.isDown("up") then
+    peeple.y = peeple.y - speed
+  elseif love.keyboard.isDown("right") then
+    peeple.x = peeple.x + speed
+  elseif love.keyboard.isDown("down") then
+    peeple.y = peeple.y + speed
+  end 
 end
  
 function love.draw()
-  love.graphics.print("Hello!",400,300)
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
+  if peeple.is_evil then
+    love.graphics.draw(evil_img, peeple.x, peeple.y, 0, 0.02)
+  else
+    love.graphics.draw(normal_img, peeple.x, peeple.y, 0, 0.02)
+  end
 end
 
